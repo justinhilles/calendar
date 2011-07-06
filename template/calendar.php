8b8479e0ae69675628fae910bf50c2bb22fcbe12
Justin Hilles <justin.hilles@gmail.com>
2011-07-05 18:32:19 -0700

    calendar

--- template/calendar.php
+++ template/calendar.php
@@ -1,4 +1,5 @@
-<h2>Next 5 Upcoming Events</h2>
+<?php if(!isset($hide_upcoming)):?>
+
 <?php // Get RSS Feed(s)
 include_once(ABSPATH . WPINC . '/feed.php');
 
@@ -6,13 +7,12 @@ include_once(ABSPATH . WPINC . '/feed.php');
 $rss = fetch_feed($feed);
 if (!is_wp_error( $rss ) ) : // Checks that the object is created correctly
     // Figure out how many total items there are, but limit it to 5.
-    $maxitems = $rss->get_item_quantity(5);
+    $maxitems = $rss->get_item_quantity(isset($limit) ?$limit:5);
 
     // Build an array of all the items, starting with element 0 (first element).
     $rss_items = $rss->get_items(0, $maxitems);
 endif;
 ?>
-
 <ul>
     <?php if ($maxitems == 0) echo '<li>No items.</li>';
     else
@@ -25,7 +25,10 @@ endif;
     <?php endforeach; ?>
 </ul>
 <br /><br />
+<?php endif;?>
 
+<?php if(!isset($hide_calendar)):?>
+<div id='calendar'></div>
 <script type="text/javascript" src="<?php echo bloginfo('siteurl') . $this -> PLUGINURL; ?>/public/plugins/fullcalendar/jquery/jquery.js"></script>
 <script type="text/javascript" src="<?php echo bloginfo('siteurl') . $this -> PLUGINURL; ?>/public/plugins/fullcalendar/jquery/ui.core.js"></script>
 <script type="text/javascript" src="<?php echo bloginfo('siteurl') . $this -> PLUGINURL; ?>/public/plugins/fullcalendar/jquery/ui.draggable.js"></script>
@@ -33,7 +36,7 @@ endif;
 <script type="text/javascript" src="<?php echo bloginfo('siteurl') . $this -> PLUGINURL; ?>/public/plugins/fullcalendar/fullcalendar.min.js"></script>
 <script type="text/javascript" src="<?php echo bloginfo('siteurl') . $this -> PLUGINURL; ?>/public/plugins/fullcalendar/gcal.js"></script>
 <script type="text/javascript">
-  $(document).ready(function() {
+  $(function() {
     $('#calendar').fullCalendar({
       events: $.fullCalendar.gcalFeed('<?php echo $feed;?>'),
       eventClick: function(event) {
@@ -51,7 +54,7 @@ endif;
     });
   });
 </script>
-<div id='loading' style='display:none'>loading...</div>
-<div id='calendar'></div>
+<?php endif;?>
+
 
 
