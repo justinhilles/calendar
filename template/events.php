b7c3272d47f56ef9689a63680c419a0dc4280490
Justin Hilles <justin.hilles@gmail.com>
2011-07-26 07:16:51 -0700

    calendar

--- template/events.php
+++ template/events.php
@@ -3,38 +3,10 @@ ini_set('display_errors', false);
 include_once(ABSPATH . WPINC . '/class-simplepie.php');
 require_once dirname(__FILE__).'/../lib/simplepie-gcalendar.php';
 
-
-$url = $_GET["feedurl"];
-$email = $_GET["email"];
-$show_past_events = $_GET["past"];
-$sort_ascending = $_GET["asc"];
-$order_by = $_GET["order"];
-$expand_single_events = $_GET["expand"];
-$language = $_GET["lang"];
-$query = $_GET["query"];
-$start = $_GET["start"];
-$end = $_GET["end"];
-$max = $_GET["max"];
-$projection = $_GET["projection"];
-$timezone = $_GET["tz"];
-
 // Get a SimplePie feed object from the specified feed source.
 $rss = new SimplePie_GCalendar();
-// $rsss->set_show_past_events($show_past_events==1);
-// $rss->set_sort_ascending($sort_ascending==1);
-// $rss->set_orderby_by_start_date($order_by==1);
-// $rss->set_expand_single_events($expand_single_events==1);
-// $rss->set_max_events($max);
-// $rss->set_cal_language($language);
-// $rss->set_projection($projection);
-// $rss->set_timezone($timezone);
-// $rss->enable_cache(false);
-// $rss->set_cache_duration(0);
-// $rss->set_cal_query($query);
-
 $rss->set_feed_url($feed);
-$rss->set_max_events(3);
-$rss->enable_order_by_date(FALSE);
+$rss->set_max_events($limit);
 $rss->init();
 
 $rss->handle_content_type();
