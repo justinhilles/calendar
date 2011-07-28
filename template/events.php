89ca72bc8a4be92f18cf38bcd33d04f0531b39b1
Justin Hilles <justin.hilles@gmail.com>
2011-07-27 19:03:05 -0700

    calendar

--- template/events.php
+++ template/events.php
@@ -1,10 +1,14 @@
 <?php // Get RSS Feed(s)
-ini_set('display_errors', false);
+//ini_set('display_errors', false);
 include_once(ABSPATH . WPINC . '/class-simplepie.php');
 require_once dirname(__FILE__).'/../lib/simplepie-gcalendar.php';
 
 // Get a SimplePie feed object from the specified feed source.
 $rss = new SimplePie_GCalendar();
+$rss->set_show_past_events(false);
+$rss->set_sort_ascending(true);
+$rss->set_orderby_by_start_date(true);
+$rss->enable_cache(false);
 $rss->set_feed_url($feed);
 $rss->set_max_events($limit);
 $rss->init();
@@ -24,7 +28,8 @@ $rss->handle_content_type();
             </div>
             <div class="description">
             <h3><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h3>
-            <p>The Little Italy Association (LIA) and a small group of community members organized a festival that celebrates Italian culture not only in the entertainment provided, on the one stage, but the types of vendors that were allowed to display their wares along India Street.</p>
+						<p><?php echo $item->get_location();?></p>
+            <p><?php echo $item->get_description();?></p>
             </div>
           </div>
         </li>
